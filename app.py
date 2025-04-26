f2c8291e9cc98fd23116fa605d609944341d926f
Gream <94147946+igream@users.noreply.github.com>
2025-04-26 13:09:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ dispositivo_id = "ESP32_01"
 def generar_dato(timestamp_actual):
     hora_actual = timestamp_actual.hour + timestamp_actual.minute / 60.0
 
-    temp_min = 9    
+    temp_min = 6   
     temp_max = 27 
     if 6 <= hora_actual <= 15:
         progreso = (hora_actual - 6) / (15 - 6)  
