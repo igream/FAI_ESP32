0b9e7b535e534aee3d7dabb8b7a9f1a9757a01aa
Gream <94147946+igream@users.noreply.github.com>
2025-04-26 13:08:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from flask import Flask, jsonify
 from pymongo import MongoClient
 from threading import Thread
 
-# Crear app de Flask
+
 app = Flask(__name__)
 
-# Configurar conexión a MongoDB
+
 mongo_uri = os.getenv('MONGO_URI')
 client = MongoClient(mongo_uri)
 db = client['BasePryEsp32']
@@ -22,21 +22,27 @@ dispositivo_id = "ESP32_01"
 def generar_dato(timestamp_actual):
     hora_actual = timestamp_actual.hour + timestamp_actual.minute / 60.0
 
-    # Simular temperatura
-    temperatura_base = 16 + 11 * math.sin((math.pi / 12) * (hora_actual - 6))
-    temperatura = round(random.normalvariate(temperatura_base, 1.0), 1)
-    temperatura = max(5.0, min(27.0, temperatura))
+    temp_min = 9    
+    temp_max = 27 
+    if 6 <= hora_actual <= 15:
+        progreso = (hora_actual - 6) / (15 - 6)  
+        temperatura = temp_min + (temp_max - temp_min) * progreso
+    else:
+        if hora_actual > 15:
+            progreso = (hora_actual - 15) / (24 - 15 + 6) 
+        else:  
+            progreso = (hora_actual + 9) / (24 - 15 + 6)
+        temperatura = temp_max - (temp_max - temp_min) * progreso
 
-    # Humedad baja
+    temperatura += random.normalvariate(0, 0.8)
+    temperatura = round(max(5.0, min(27.0, temperatura)), 1)
     humedad = round(random.uniform(0.0, 7.0), 1)
 
-    # Luz basada en hora
     if 6 <= timestamp_actual.hour <= 18:
         luz = random.randint(1500, 4000)
     else:
         luz = random.randint(0, 800)
 
-    # Movimiento con baja probabilidad
     movimiento = 1 if random.random() < 0.05 else 0
 
     return {
@@ -49,6 +55,7 @@ def generar_dato(timestamp_actual):
         "timestamp": timestamp_actual.isoformat()
     }
 
+
 def simulador():
     print("Simulador iniciado... Enviando datos a MongoDB cada 60 segundos.")
     while True:
@@ -58,21 +65,13 @@ def simulador():
         print(f"[{ahora}] Dato insertado: {dato}")
         time.sleep(60)
 
-# Ruta raíz
-@app.route('/')
-def home():
-    return 'API Flask con MongoDB funcionando en Render', 200
-    
 @app.route("/api/datos", methods=["GET"])
 def obtener_datos():
-    datos = list(collection.find().sort("timestamp", -1).limit(100))  # Últimos 100 datos
+    datos = list(collection.find().sort("timestamp", -1).limit(100))  
     for dato in datos:
-        dato["_id"] = str(dato["_id"])  # Convertir ObjectId a string
+        dato["_id"] = str(dato["_id"])  
     return jsonify(datos)
 
 if __name__ == "__main__":
-    # Ejecutar simulador en segundo plano
     Thread(target=simulador, daemon=True).start()
-
-    # Correr el servidor Flask
     app.run(host="0.0.0.0", port=10000)
